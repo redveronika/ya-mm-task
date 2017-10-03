d594acd6117ff47a790e3d555641e69fb28a6f74
Veronika Voronova <voronovaveronika@gmail.com>
2017-10-03 20:08:35 +0300

    Подключила Redux, сделал сохранение статистики времени посещения вкладки rating

--- app/index.js
+++ app/index.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import reducer from './reducers';
 
 import { Main } from '../app/blocks';
 
 import './styles.css';
 import './assets/fonts/fonts.css';
 
+const store = createStore(reducer, composeWithDevTools());
+
 const component = () => {
     render(
-        <Router>
-            <Switch>
-                <Route
-                    path="/main/:filter?"
-                    render={({ match }) => (
-                        <Main filter={match.params.filter} />
-                    )}
-                />
-                <Redirect from="/" to="/main" />
-            </Switch>
-        </Router>,
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route
+                        path="/main/:filter?"
+                        render={({ match }) => (
+                            <Main filter={match.params.filter} />
+                        )}
+                    />
+                    <Redirect from="/" to="/main" />
+                </Switch>
+            </Router>
+        </Provider>,
         document.getElementById('app'),
     );
 };
