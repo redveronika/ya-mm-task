26a2385ac064db57035399e3722066fe1eb3bc9b
Veronika Voronova <voronovaveronika@gmail.com>
2017-10-04 12:21:13 +0300

    Создала reducer для табов, перенесла все табы в стор

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as tabs } from './tabs.reducer';
 import { reducer as rating } from './rating.reducer';
 
 export default combineReducers({
+    tabs,
     rating,
 });
