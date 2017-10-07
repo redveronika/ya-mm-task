f34cf978ffe70249e47e0b2c1de2b147cdac6e2d
Veronika Voronova <voronovaveronika@gmail.com>
2017-10-07 15:45:32 +0300

    Добавила prod конфиг вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,9 @@
-const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+'use strict';
 
-module.exports = {
-    entry: {
-        app: './app/index.js',
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-        historyApiFallback: true,
-        stats: 'errors-only',
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    'css-loader'],
-            },
-            {
-                test: /\.(woff|woff2)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'assets/fonts/',
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/imgs/',
-                    },
-                }],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlPlugin({
-            template: './app/index.html',
-        }),
-        new CleanWebpackPlugin(['dist']),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
+const path = require('path');
+
+module.exports = function (env) {
+    return require(
+        path.join(__dirname, 'webpack/', `webpack.${env}.config.js`)
+    );
 };
