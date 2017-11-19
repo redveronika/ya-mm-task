0f25d9361f1736c4ed2e8094164b2e14a5771c67
Veronika Voronova <voronovaveronika@gmail.com>
2017-11-19 21:10:28 +0300

    Переписала тесты кнопок, добавила тесты для кнопки типа «light»

--- app/tests/gemini/button.js
+++ app/tests/gemini/button.js
@@ -1,40 +1,26 @@
 gemini.suite('button', (suite) => {
     suite
-        .setUrl('./button')
+        .setUrl('./button');
 
-    gemini.suite('default button', (defaultButton) => {
-        defaultButton
-            .setCaptureElements('.button--default')
-            .before((actions, find) => {
-                this.button = find('.button--default');
-            })
-            .capture('plain')
-            .capture('hovered', (actions) => {
-                actions.mouseMove(this.button);
-            })
-            .capture('pressed', (actions) => {
-                actions.mouseDown(this.button);
-            })
-            .capture('clicked', (actions) => {
-                actions.mouseUp(this.button);
-            });
-    });
+    const buttonsToTest = ['.button--default', '.button--light', '.button--bright'];
 
-    gemini.suite('bright button', (brightButton) => {
-        brightButton
-            .setCaptureElements('.button--bright')
-            .before((actions, find) => {
-                this.button = find('.button--bright');
-            })
-            .capture('plain')
-            .capture('hovered', (actions) => {
-                actions.mouseMove(this.button);
-            })
-            .capture('pressed', (actions) => {
-                actions.mouseDown(this.button);
-            })
-            .capture('clicked', (actions) => {
-                actions.mouseUp(this.button);
-            });
+    buttonsToTest.forEach((button) => {
+        gemini.suite(`${button} capturing`, (element) => {
+            element
+                .setCaptureElements(button)
+                .before((actions, find) => {
+                    this.button = find(button);
+                })
+                .capture('plain')
+                .capture('hovered', (actions) => {
+                    actions.mouseMove(this.button);
+                })
+                .capture('pressed', (actions) => {
+                    actions.mouseDown(this.button);
+                })
+                .capture('clicked', (actions) => {
+                    actions.mouseUp(this.button);
+                });
+        });
     });
 });
