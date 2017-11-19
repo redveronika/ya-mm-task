928935f086b33831c3496031b82e9609af9156d9
Veronika Voronova <voronovaveronika@gmail.com>
2017-11-19 16:06:17 +0300

    Переписала конфиг в yml-файл, добавила тесты для кнопки типа «bright», добавила скриншоты в репозиторий

--- app/tests/gemini/button.js
+++ app/tests/gemini/button.js
@@ -1,18 +1,40 @@
 gemini.suite('button', (suite) => {
     suite
         .setUrl('./button')
-        .setCaptureElements('.button')
-        .before((actions, find) => {
-            this.button = find('.button--default');
-        })
-        .capture('plain')
-        .capture('hovered', (actions) => {
-            actions.mouseMove(this.button);
-        })
-        .capture('pressed', (actions) => {
-            actions.mouseDown(this.button);
-        })
-        .capture('clicked', (actions) => {
-            actions.mouseUp(this.button);
-        });
+
+    gemini.suite('default button', (defaultButton) => {
+        defaultButton
+            .setCaptureElements('.button--default')
+            .before((actions, find) => {
+                this.button = find('.button--default');
+            })
+            .capture('plain')
+            .capture('hovered', (actions) => {
+                actions.mouseMove(this.button);
+            })
+            .capture('pressed', (actions) => {
+                actions.mouseDown(this.button);
+            })
+            .capture('clicked', (actions) => {
+                actions.mouseUp(this.button);
+            });
+    });
+
+    gemini.suite('bright button', (brightButton) => {
+        brightButton
+            .setCaptureElements('.button--bright')
+            .before((actions, find) => {
+                this.button = find('.button--bright');
+            })
+            .capture('plain')
+            .capture('hovered', (actions) => {
+                actions.mouseMove(this.button);
+            })
+            .capture('pressed', (actions) => {
+                actions.mouseDown(this.button);
+            })
+            .capture('clicked', (actions) => {
+                actions.mouseUp(this.button);
+            });
+    });
 });
