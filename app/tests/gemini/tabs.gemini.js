f2a8debcb95c285e74c2fbf35f7c4ee3c482c73c
redveronika <voronovaveronika@gmail.com>
2017-12-10 20:47:33 +0300

    Изменила аргументы gemini теста на suite, отключила правило линтера no-shadow для gemini-тестов, поправила команду lint

--- app/tests/gemini/tabs.gemini.js
+++ app/tests/gemini/tabs.gemini.js
@@ -1,20 +1,20 @@
 gemini.suite('tabs', (suite) => {
     suite.setUrl('./');
 
-    gemini.suite('tabs', (tabs) => {
-        tabs.setCaptureElements('.tabs')
+    gemini.suite('tabs', (suite) => {
+        suite.setCaptureElements('.tabs')
             .capture('plain');
     });
 
-    gemini.suite('tab__link', (tab) => {
-        tab
+    gemini.suite('tab__link', (suite) => {
+        suite
             .setCaptureElements('.tab__link:not(.tab__link--active)')
             .capture('plain');
     });
 
-    gemini.suite('tab__link--active', (tab) => {
+    gemini.suite('tab__link--active', (suite) => {
         const selector = '.tab__link--active';
-        tab
+        suite
             .setCaptureElements(selector)
             .before((actions, find) => {
                 this.tab = find(selector);
